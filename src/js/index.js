f4fdac758994c6d36e8ad6fc8bef21e09f7c14bf
Tommy <tommy128works@gmail.com>
2023-07-19 08:32:57 -0400

    feat: add error message to search box
    If the user enters an invalid location, an error message will be
    displayed.

--- src/js/index.js
+++ src/js/index.js
@@ -35,82 +35,85 @@ const loadPage = async (location) => {
       getForecastWeatherData(location),
     ]);
 
-    document.body.innerHTML = "";
-    let topPanel = document.createElement("div");
-    topPanel.classList.add("panel-container", "top-panel");
+    if (!Object.hasOwn(currentData, "error")) {
+      document.body.innerHTML = "";
+      let topPanel = document.createElement("div");
+      topPanel.classList.add("panel-container", "top-panel");
 
-    let localTime = currentData.location.localtime.split(" ");
+      let localTime = currentData.location.localtime.split(" ");
 
-    let leftPanel = createControlCenter(
-      currentData.current.condition.text,
-      currentData.location.name + ", " + currentData.location.region,
-      localTime[0],
-      localTime[1],
-      currentData.current.temp_c,
-      currentData.current.temp_f,
-      currentData.current.condition.icon
-    );
-    leftPanel.classList.add("left-panel");
-    topPanel.appendChild(leftPanel);
+      let leftPanel = createControlCenter(
+        currentData.current.condition.text,
+        currentData.location.name + ", " + currentData.location.region,
+        localTime[0],
+        localTime[1],
+        currentData.current.temp_c,
+        currentData.current.temp_f,
+        currentData.current.condition.icon
+      );
+      leftPanel.classList.add("left-panel");
+      topPanel.appendChild(leftPanel);
 
-    let rightPanel = createWeatherDetails(
-      currentData.current.feelslike_c,
-      currentData.current.feelslike_f,
-      currentData.current.humidity,
-      currentData.current.precip_mm,
-      currentData.current.precip_in,
-      currentData.current.wind_kph,
-      currentData.current.wind_mph,
-      currentData.current.uv,
-      forecastData.forecast.forecastday[0].astro.sunrise,
-      forecastData.forecast.forecastday[0].astro.sunset
-    );
-    rightPanel.classList.add("right-panel");
-    topPanel.appendChild(rightPanel);
+      let rightPanel = createWeatherDetails(
+        currentData.current.feelslike_c,
+        currentData.current.feelslike_f,
+        currentData.current.humidity,
+        currentData.current.precip_mm,
+        currentData.current.precip_in,
+        currentData.current.wind_kph,
+        currentData.current.wind_mph,
+        currentData.current.uv,
+        forecastData.forecast.forecastday[0].astro.sunrise,
+        forecastData.forecast.forecastday[0].astro.sunset
+      );
+      rightPanel.classList.add("right-panel");
+      topPanel.appendChild(rightPanel);
 
-    document.body.appendChild(topPanel);
+      document.body.appendChild(topPanel);
 
-    let bottomPanel = document.createElement("div");
-    bottomPanel.classList.add("bottom-panel");
+      let bottomPanel = document.createElement("div");
+      bottomPanel.classList.add("bottom-panel");
 
-    let hourlyData = filterForecastDataForHourlyWeather(forecastData);
-    bottomPanel.appendChild(
-      createHourlyWeather(
-        hourlyData.hours,
-        hourlyData.weatherIcons,
-        hourlyData.tempsC,
-        hourlyData.tempsF,
-        hourlyData.rainChance,
-        hourlyData.snowChance
-      )
-    );
+      let hourlyData = filterForecastDataForHourlyWeather(forecastData);
+      bottomPanel.appendChild(
+        createHourlyWeather(
+          hourlyData.hours,
+          hourlyData.weatherIcons,
+          hourlyData.tempsC,
+          hourlyData.tempsF,
+          hourlyData.rainChance,
+          hourlyData.snowChance
+        )
+      );
 
-    let dailyData = filterForecastDataForDailyWeather(forecastData);
-    bottomPanel.appendChild(
-      createDailyWeather(
-        dailyData.dates,
-        dailyData.weatherIcons,
-        dailyData.rainChances,
-        dailyData.snowChances,
-        dailyData.maxTempsC,
-        dailyData.minTempsC,
-        dailyData.maxTempsF,
-        dailyData.minTempsF
-      )
-    );
+      let dailyData = filterForecastDataForDailyWeather(forecastData);
+      bottomPanel.appendChild(
+        createDailyWeather(
+          dailyData.dates,
+          dailyData.weatherIcons,
+          dailyData.rainChances,
+          dailyData.snowChances,
+          dailyData.maxTempsC,
+          dailyData.minTempsC,
+          dailyData.maxTempsF,
+          dailyData.minTempsF
+        )
+      );
 
-    document.body.appendChild(bottomPanel);
-    addDragToScroll("hourly-weather-container");
+      document.body.appendChild(bottomPanel);
+      addDragToScroll("hourly-weather-container");
 
-    document.body.appendChild(createFooter());
+      document.body.appendChild(createFooter());
 
-    addEventListenersToSearchBox("location");
+      addEventListenersToSearchBox("location");
+    } else {
+      let searchBoxMessage = document.getElementById("search-box-message");
+      searchBoxMessage.textContent =
+        'Location not found. Search must be in the form of "City", "City, State" or "City, Country"';
+    }
   } catch (error) {
     console.log(error);
     console.log("Unable to retrieve weather data");
-    // let searchBoxMessage = document.getElementById("search-box-message");
-    // searchBoxMessage.textContent =
-    //   'Location not found. Search must be in the form of "City", "City, State" or "City, Country"';
   }
 };
 
